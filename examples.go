b40dd3420ec08e3ca30a1bf6292096f298524acc
Harrison Saliba <harrysaliba4139@gmail.com>
2021-12-28 19:30:27 -0800

    Added function and concurrency examples.

--- examples.go
+++ examples.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -65,13 +67,133 @@ func main() {
 	myMap["My Favorite Number"] = 2
 	myMap["My Second Favorite Number"] = 4
 	myMap["My SSN"] = 1234567890
-	delete("My SSN")
+	delete(myMap, "My SSN")
 	fmt.Println(myMap)
 
-	// set loop
-
+	// loop over a set
+	for myIndex, myValue := range myArray { // also works for value only (for v := range myArray) and maps
+		fmt.Println("look, i found a", myValue, "at position", myIndex)
+	}
 
 	// functions
-	// NOTE: variadic functions can be made using func() (...int) and if you are passing a slice
-	// and you can send the elements as separate arguments as: myFunction(mySlice...)
+	fmt.Println("let's ask my friend, sum, to add these numbers, he says:", sum(9, 10))
+
+	// multi return functions
+	mul, div := multiplyAndDivide(8, 4) // if i did not need a return, i can simply sure _ instead of a variable name
+	fmt.Println("my other friend, multiplyAndDivide is very good at giving me all the info:", mul, "and", div)
+
+	// varadic functions
+	fmt.Println("I can send this function soooo many arguments:", sumAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) // and you can send the elements as separate arguments as: myFunction(mySlice...)
+
+	// closures & returned functinos
+	anotherFunction := myNumberFunction()
+	fmt.Println(anotherFunction())
+	fmt.Println(anotherFunction())
+	fmt.Println(anotherFunction())
+
+	// inline functions
+	printSilly := func() {
+		fmt.Println("silly")
+	}
+	printSilly()
+
+	// structs
+	bigBoy := train{1200, 24, 1000000, "UP 4014"}
+	fmt.Println("Look at this train:", bigBoy)
+
+	// methods
+	fmt.Println("it can pull like:", bigBoy.traction())
+
+	// interfaces are supported and look like:
+	// type vechicle interface{
+	// 	getHorepower() int
+	// }
+	// but I dont use interfaces that much
+
+	// concurrency & parraelleism & wait groups
+	fmt.Println("Let's race and see who can count to 20 the fastest!")
+	var myWaitGroup sync.WaitGroup
+	racer := func() {
+		for i := 1; i <= 20; i++ {
+			fmt.Print(i, " ")
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+	startRacer := func() {
+		racer()
+		myWaitGroup.Done()
+	}
+	myWaitGroup.Add(3)
+	go startRacer()
+	go startRacer()
+	go startRacer()
+	myWaitGroup.Wait()
+	fmt.Println()
+
+	// channels
+	myChannel := make(chan int)
+	producer := func(count int, c chan int) {
+		for i := 0; i < count; i++ {
+			fmt.Println("I'm making a", i)
+			fmt.Println("I'm sending a", i)
+			c <- i
+		}
+	}
+	consumer := func(count int, c chan int) {
+		for i := 0; i < count; i++ {
+			food := <- c
+			fmt.Println("I got a", food)
+		}
+	}
+	myWaitGroup.Add(2)
+	go func() {
+		producer(5, myChannel)
+		myWaitGroup.Done()
+	}()
+	go func() {
+		consumer(5, myChannel)
+		myWaitGroup.Done()
+	}()
+	myWaitGroup.Wait()
+}
+
+// functions
+func sum(a int, b int) int {
+	return a + b
+}
+
+// multiple return functions
+func multiplyAndDivide(a int, b int) (int, int) {
+	return a * b, a / b
+}
+
+// varadic functions
+func sumAll(n ...int) int {
+	sum := 0
+	for v := range n {
+		sum += v
+	}
+	return sum
+}
+
+// closures
+func myNumberFunction() func() int {
+	mySuperSpecialNumber := 10
+	return func() int {
+		mySuperSpecialNumber++
+		return mySuperSpecialNumber
+	}
+}
+
+// structs
+type train struct { // you can also embeed another struct within a struct (think JSON)
+	power int
+	wheels int
+	weight int
+	name string
+}
+
+// methods
+func (t train) traction() int {
+	return t.weight * t.power
 }
